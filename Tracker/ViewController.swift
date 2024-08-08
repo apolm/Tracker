ca311af54e092b818a89a81d50b44325ad550c79
Artem Polivanov <polivanov.a@yandex.ru>
2024-08-08 09:46:01 +0300

    Remove Main.storyboard Edit LaunchScreen Add colors, configure folders

--- Tracker/ViewController.swift
+++ Tracker/ViewController.swift
@@ -1,17 +1,20 @@
-//
-//  ViewController.swift
-//  Tracker
-//
-//  Created by Artem Polivanov on 08.08.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 
