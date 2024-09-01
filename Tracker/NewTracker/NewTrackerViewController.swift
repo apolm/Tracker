a2a265da5a62f93f39f5444d77fa908e0f94f58e
Artem Polivanov <polivanov.a@yandex.ru>
2024-09-01 15:59:30 +0300

    Add New Tracker View

--- Tracker/NewTracker/NewTrackerViewController.swift
+++ Tracker/NewTracker/NewTrackerViewController.swift
@@ -1,8 +1,78 @@
 import UIKit
 
 final class NewTrackerViewController: AddTrackerFlowViewController {
+    // MARK: - Public Properties
     let isRegular: Bool
     
+    // MARK: - Private Properties
+    lazy var tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.separatorStyle = .none
+        table.backgroundColor = .ypWhite
+        table.delegate = self
+        table.dataSource = self
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+    
+    lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Введите название трекера"
+        textField.font = .systemFont(ofSize: 17, weight: .regular)
+        textField.clearButtonMode = .whileEditing
+        textField.borderStyle = .none
+        textField.backgroundColor = .clear
+        textField.translatesAutoresizingMaskIntoConstraints = false
+                
+        return textField
+    }()
+    
+    lazy var categoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Категория"
+        label.font = .systemFont(ofSize: 17, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var scheduleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Расписание"
+        label.font = .systemFont(ofSize: 17, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var cancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Отменить", for: .normal)
+        button.setTitleColor(.ypRed, for: .normal)
+        button.backgroundColor = .ypWhite
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.layer.cornerRadius = 16
+        button.layer.masksToBounds = true
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.ypRed.cgColor
+        button.addTarget(self, action: #selector(cancelButtonDidTap), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    lazy var createButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Создать", for: .normal)
+        button.setTitleColor(.ypWhite, for: .normal)
+        button.backgroundColor = .ypGray
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.layer.cornerRadius = 16
+        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(createButtonDidTap), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // MARK: - Public Methods
     init(isRegular: Bool) {
         self.isRegular = isRegular
         super.init(nibName: nil, bundle: nil)
@@ -12,9 +82,153 @@ final class NewTrackerViewController: AddTrackerFlowViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(tableView)
+        view.addSubview(cancelButton)
+        view.addSubview(createButton)
+        setupConstraints()
+        
         title = isRegular ? "Новая привычка" : "Новое нерегулярное событие"
+    }
+    
+    // MARK: - Private Methods
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: cancelButton.topAnchor, constant: -16),
+            
+            cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            cancelButton.trailingAnchor.constraint(equalTo: createButton.leadingAnchor, constant: -8),
+            cancelButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            cancelButton.heightAnchor.constraint(equalToConstant: 60),
+            
+            createButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            createButton.bottomAnchor.constraint(equalTo: cancelButton.bottomAnchor),
+            createButton.widthAnchor.constraint(equalTo: cancelButton.widthAnchor),
+            createButton.heightAnchor.constraint(equalTo: cancelButton.heightAnchor)
+        ])
+    }
+    
+    // MARK: - Actions
+    @objc private func cancelButtonDidTap() {
+        
+    }
+    
+    @objc private func createButtonDidTap() {
+        
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension NewTrackerViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? 1 : 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.selectionStyle = .none
+        cell.backgroundColor = .ypBackground
+        cell.contentView.subviews.forEach { $0.removeFromSuperview() }
+        
+        if indexPath.section == 0 {
+            cell.contentView.addSubview(textField)
+            cell.layer.cornerRadius = 16
+            cell.layer.masksToBounds = true
+            
+            NSLayoutConstraint.activate([
+                textField.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 16),
+                textField.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: -12),
+                textField.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
+                textField.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor)
+            ])
+        } else {
+            cell.accessoryType = .disclosureIndicator
+            
+            if indexPath.row == 0 {
+                cell.contentView.addSubview(categoryLabel)
+                
+                let separator = UIView()
+                separator.backgroundColor = .ypGray
+                separator.translatesAutoresizingMaskIntoConstraints = false
+                cell.contentView.addSubview(separator)
+                
+                NSLayoutConstraint.activate([
+                    categoryLabel.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 16),
+                    categoryLabel.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: -16),
+                    categoryLabel.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
+                    categoryLabel.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor),
+                    
+                    separator.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 16),
+                    separator.widthAnchor.constraint(equalToConstant: cell.frame.width - 32),
+                    separator.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor),
+                    separator.heightAnchor.constraint(equalToConstant: 0.5)
+                ])
+                
+                cell.layer.cornerRadius = 16
+                cell.layer.masksToBounds = true
+                cell.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            } else if indexPath.row == 1 {
+                cell.contentView.addSubview(scheduleLabel)
+                NSLayoutConstraint.activate([
+                    scheduleLabel.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor, constant: 16),
+                    scheduleLabel.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor, constant: -16),
+                    scheduleLabel.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
+                    scheduleLabel.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor)
+                ])
+            }
+            
+            if indexPath.row == tableView.numberOfRows(inSection: indexPath.section) - 1 {
+                cell.layer.cornerRadius = 16
+                cell.layer.masksToBounds = true
+                cell.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            }
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 24
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView()
+        footerView.backgroundColor = UIColor.clear
+        return footerView
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension NewTrackerViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.section == 1 && indexPath.row == 1 {
+            let viewController = ScheduleViewController()
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        75.0
     }
 }
---
+++ Tracker/NewTracker/ScheduleViewController.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+final class ScheduleViewController: AddTrackerFlowViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel()
+        label.text = "Test"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        let button = UIButton()
+        button.setTitle("Go Back", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        title = "Test"
+    }
+    
+    @objc private func buttonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+}
