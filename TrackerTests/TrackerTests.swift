1c90aae09b138f11145b67937d7ec727ada1703c
Artem Polivanov <polivanov.a@yandex.ru>
2024-09-27 07:25:47 +0300

    Add Snapshot Testing package

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+
+final class TrackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+}
